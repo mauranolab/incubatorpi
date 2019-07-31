3c98e9cdb5862cede4c463003a6914ab90ff0113
mattmaurano <44711377+mattmaurano@users.noreply.github.com>
2019-07-31 10:27:34 -0400

    Update incubatorpi.py
    Change alarm logic to report change in individual incubator status
    Add uptime counter to messages
    Separate config into text file
    Add extra sleeps to throttle email in case of errors
    Reconfigure startup sequence to facilitate logging early errors
    Fix shell directive to specify python3. Confusingly the crontab invoked python3 but pi had python 2 in path

--- incubatorpi.py
+++ incubatorpi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import RPi.GPIO as IO
 import time
@@ -8,12 +8,8 @@ import signal
 import sys
 import logging
 from logging.handlers import TimedRotatingFileHandler
+import configparser
 
-
-###Global parameters to be configured per alarm
-alarmname = 'Maurano_SB848'
-to_emails = ['maurano@nyu.edu', 'ranbroshran@gmail.com', 'martij44@nyu.edu']
-incubatornames = ['Incubator #1', 'Incubator #2', 'Incubator #3', 'Incubator #4']
 
 ###Utility functions
 def led_blink(color,state):
@@ -29,6 +25,8 @@ def led_blink(color,state):
 
 #Will pass on any exceptions for send failure
 def send_email(user, recipient, subject, body):
+    global smtpserver
+    
     FROM = user
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
@@ -39,59 +37,89 @@ def send_email(user, recipient, subject, body):
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
 #    print(message)
 #    print("trying to send email")
-    server = smtplib.SMTP("smtp.nyumc.org")
+    server = smtplib.SMTP(smtpserver)
     #server.ehlo()
     server.sendmail(FROM, TO, message)
     server.quit()
 #    print('successfully sent the mail')
+    
+    #Add in a delay to act as crude rate limiting
+    time.sleep(20)
 
 
 #%% in message will be replaced by incubator number
-def broadcast_message(logger, to_emails, alarmname, incubatornames, incubatornum, message):
-    if incubatornames is not None and incubatornum is not None:
+def broadcast_message(logger, to_emails, alarmname, incubatornames, status, incubatornum, message):
+    global startupdate
+    global senderemail
+    
+    if incubatornum is not None and incubatornames is not None:
         message = message.replace('%%', str(incubatornum) + ' (' + incubatornames[incubatornum-1] + ')')
-    body = alarmname + ' - ' + message + '\n\n' + str(datetime.now())
+    body = alarmname + ' - ' + message + '\n\n'
+    if incubatornames is not None and status is not None:
+        body += 'Current status: \n'
+        for i in range(len(status)):
+            body += incubatornames[i] + ' :' + ['ALARM', 'OK'][status[i]] + "\n"
+        body += '\n'
+    body += 'Current time: ' + str(datetime.now()) + '\n'
+    runtime = datetime.now() - startupdate
+    body += 'Running since: ' + str(startupdate) + ' (%d days, %d hours and %d minutes)\n' % (runtime.days, runtime.seconds/3600, runtime.seconds/60)
     
     logger(message)
     
-    success = False
-    while not success and len(to_emails) >= 1:
+    keeptrying = True
+    while keeptrying:
         try:
             #TODO Wrong severity level
             logger("Sending email to " + ",".join(to_emails))
-            send_email('andrew.martin@nyulangone.org', to_emails, alarmname + ' - ' + message, body)
-            success = True
+            send_email(senderemail, to_emails, alarmname + ' - ' + message, body)
+            keeptrying = False
         except Exception:
-            logger("Failed to send email to " + ",".join(to_emails))
-            success = False
-            #Try again out of paranoia after blindly truncating the last recipient from to_emails; send_email doesn't seem to check for a valid address beyond a@b.com, but just in case...
-            #NB couldn't figure out how to test this condition, putting an address invalid on its own into to_emails doesn't trigger an SMTP error
-            to_emails = to_emails[-1]
-    return success
+            msg = "Failed to send email to " + ",".join(to_emails)
+            logger(msg)
+            #tried logger.exception(msg) but get "AttributeError: 'function' object has no attribute 'exception'"
+            if len(to_emails) > 1:
+                #Try again out of paranoia after blindly truncating the first recipient from to_emails; send_email doesn't seem to check for a valid address beyond a@b.com, but just in case...
+                #NB couldn't figure out how to test this condition, putting an address invalid on its own into to_emails doesn't trigger an SMTP error
+                to_emails = to_emails[-1]
+            else:
+                #Nothing left to try
+                keeptrying = False
 
 
 ###Main code begins here
-#Setting up input pins to detect incubator alarm.
-IO.setmode(IO.BOARD)
-IO.setup(12, IO.IN, pull_up_down=IO.PUD_DOWN) #DOWN = low voltage, Incubator Alarm Contacts 2-com, 3-N.O.
-IO.setup(11, IO.IN, pull_up_down=IO.PUD_DOWN)
-IO.setup(10, IO.IN, pull_up_down=IO.PUD_DOWN)
-IO.setup(8, IO.IN, pull_up_down=IO.PUD_DOWN)
+###Initialize parameters not in the config file
+basedir='/home/pi/Alarm/' #So we can find config file and log dirs
+senderemail='andrew.martin@nyulangone.org'
+smtpserver="smtp.nyumc.org"
 
+#Initialize default values in case we have an error parsing config file later on
+alarmname = 'Unknown'
+to_emails = ['maurano@nyu.edu']
+incubatornames = ['Incubator 1', 'Incubator 2', 'Incubator 3', 'Incubator 4']
 
-#Configure logging
+###Initialize global variables
+startupdate = datetime.now()
+weeklytest = False #Whether we are in the weekly heartbeat announcement
+alarm = False #Whether we are in an alarm state
+status = [1, 1, 1, 1] # State vector of the incubators this cycle
+priorstatus = status # State vector of the incubators last cycle
+
+###Configure logging
 logger = logging.getLogger("IncubatorAlarmLog")
 logger.setLevel(logging.DEBUG)
 
-handler1 = TimedRotatingFileHandler('/home/pi/Alarm/Log/' + alarmname + ".txt", when="D", interval=30, backupCount=1000)
-handler1.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+handler1 = TimedRotatingFileHandler(basedir + 'Log/incubatoralarm.log', when="D", interval=30, backupCount=1000)
 handler1.setLevel(logging.INFO)
 logger.addHandler(handler1)
 
 handler2 = logging.StreamHandler()
-handler2.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%s'))
 logger.addHandler(handler2)
 
+#alarmname will be updated after reading config file
+handler1.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+handler2.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%s'))
+
+logger.info("Beginning startup")
 
 #Set up handler to log a shutdown notice
 def sigterm_handler(_signo, _stack_frame):
@@ -103,14 +131,50 @@ def sigterm_handler(_signo, _stack_frame):
 signal.signal(signal.SIGTERM, sigterm_handler)
 
 
-alarm = False #Whether we are in an alarm state
-alarm_check = True #Whether the alarm has been successfully reported
-weeklytest = False #Whether we are in the weekly heartbeat announcement
+###initialize configuration
+logger.info("Reading configuration from alarm_config.ini")
+#alarm_config.ini must be present in same directory:
+#[DEFAULT]
+#alarmname=MyAlarmName
+#emails=test@nyu.edu,test2@nyu.edu
+#incubatornames=Incubator 1,Incubator 2,Incubator 3,Incubator 4
+
+####Now that we have the configuration loaded add the logfile handler
+try:
+#    raise Exception("test")
+    config = configparser.ConfigParser()
+    config.read(basedir + 'alarm_config.ini')
+    
+    alarmname = config.get('DEFAULT', 'alarmname')
+    #First update logging formatters to include alarmname pulled from config file
+    handler1.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+    handler2.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s ' + alarmname + ' %(message)s', datefmt='%Y-%m-%d %H:%M:%s'))
+    
+    to_emails = config.get('DEFAULT', 'emails').split(',')
+    incubatornames = config.get('DEFAULT', 'incubatornames').split(',')
+    logger.info("alarmname=" + alarmname)
+    logger.info("to_emails=" + ",".join(to_emails))
+    logger.info("incubatornames=" + ",".join(incubatornames))
+except Exception:
+    msg = "Failed to read configuration, using hardcoded defaults"
+    logger.exception(msg)
+    #NB email may fail if run by cron since network may not be fully up yet, so give it a bit of time
+    time.sleep(60)
+    broadcast_message(logger.critical, to_emails, alarmname, None, None, None, msg)
 
 
-logger.critical('Started up successfully')
+###Setting up input pins to detect incubator alarm.
+IO.setmode(IO.BOARD)
+IO.setup(12, IO.IN, pull_up_down=IO.PUD_DOWN) #DOWN = low voltage, Incubator Alarm Contacts 2-com, 3-N.O.
+IO.setup(11, IO.IN, pull_up_down=IO.PUD_DOWN)
+IO.setup(10, IO.IN, pull_up_down=IO.PUD_DOWN)
+IO.setup(8, IO.IN, pull_up_down=IO.PUD_DOWN)
 
 
+logger.critical('Finished startup')
+
+
+###Main loop
 while True:
     try:
         ###Check whether we should send a heartbeat message
@@ -118,7 +182,7 @@ while True:
         
         #Send the heartbeat message on Mondays at 10:00am
         if my_dt_ob.weekday() == 0 and my_dt_ob.hour == 10 and weeklytest == False:
-            broadcast_message(logger.info, to_emails, alarmname, None, None, 'Weekly heartbeat - still running')
+            broadcast_message(logger.info, to_emails, alarmname, incubatornames, status, None, 'Weekly heartbeat - still running')
             weeklytest = True
         elif my_dt_ob.hour == 11:
             weeklytest = False
@@ -132,27 +196,37 @@ while True:
         #IO.input(pin) measures current status of the pin
         #in this case, it returns either 0 or 1
         #In the all good state, status = [1, 1, 1, 1]
+        priorstatus = status
         status = [IO.input(12), IO.input(11), IO.input(10), IO.input(8)]
-        logger.debug("Incubator status: " + str(status))
+        
+        #Print out status vector every 4 seconds
+        #logger.debug("Incubator status: " + str(status))
         
         if 0 not in status:
+            #All clear
             indicator = 'green'
-            if alarm == True and alarm_check == True:
-                broadcast_message(logger.critical, to_emails, alarmname, incubatornames, alarm_number, 'Incubator #%% has recovered')
+            if alarm == True:
+                #We have just gone from 1+ incubator being in alarm state to all clear
+                broadcast_message(logger.critical, to_emails, alarmname, incubatornames, status, None, 'All incubators have recovered')
                 alarm = False #updating previous alarm state to clear
         else:
+            #1+ incubator is in alarm state
             indicator = 'red'
-            for i in range(4):
-                if status[i] == 0: #checking for current alarm state
-                    if alarm == False: #check for previous alarm state
-                        alarm_number = i+1
-                        alarm_check = broadcast_message(logger.critical, to_emails, alarmname, incubatornames, alarm_number, 'Alarm detected in incubator #%%')
-                        alarm = True #updating previous alarm state to alarmed
+            alarm = True #updating previous alarm state to alarmed
+            for i in range(len(status)):
+                if status[i] != priorstatus[i]:
+                    alarm_number = i+1
+                    if status[i] == 0: # This incubator just failed
+                        broadcast_message(logger.critical, to_emails, alarmname, incubatornames, status, alarm_number, 'New alarm detected in incubator #%%')
+                    if status[i] == 1: # This incubator just recovered (another incubator must still be failed)
+                        broadcast_message(logger.critical, to_emails, alarmname, incubatornames, status, alarm_number, 'Incubator #%% has recovered')
         
-        led_blink(indicator,1)
-        time.sleep(2)
-        led_blink(indicator,0)
-        time.sleep(2)
+        #Sleep for 60 seconds to throttle the main control loop
+        for i in range(0, 10):
+            led_blink(indicator,1)
+            time.sleep(3)
+            led_blink(indicator,0)
+            time.sleep(3)
     except Exception:
         logger.exception("Fatal error in main loop")
 
